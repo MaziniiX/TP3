16bb0cb577aaf8890f83bb4aa09c5eaa8912803e
Justin Chaleard <justin.chaleard@uha.fr>
2022-11-24 11:50:44 +0100

    Initial commit

---
+++ tp3exo1.py
@@ -0,0 +1,55 @@
+
+#Exercice A
+#n=int(input("Veuillez saisir un nombre entier naturel:"))
+#somme=0
+#for n in range(n+1):
+#    somme=somme+n
+#print(somme)
+
+#Exercice B
+#wait=0
+#while wait!=100:
+#    wait=int(input("Veuillez saisir la valeur 100 pour continuer :"))
+#print("Fin")
+
+#Exercice C
+#valeur=0
+#inften=0
+#equalten=0
+#equalfifteen=0
+#valeursaisie=0
+#for valeur in range(10):
+#    valeur=int(input("Veuillez saisir un nombre compris entre 0 et 20"))
+#    while valeur<0 or valeur>20:
+#        valeur=int(input("Veuillez saisir un nombre compris entre 0 et 20"))
+#    valeursaisie=valeursaisie+1
+#    print(valeursaisie)
+#    if valeur<10:
+#        inften=inften+1
+#    if valeur>=10:
+#        equalten=equalten+1
+#    if valeur>=15:
+#        equalfifteen=equalfifteen+1
+#print("Il y a {} nombres inférieurs à 10, {} nombres superieurs ou égales à 10, {} nombres supérieurs ou égales à 15".format(inften, equalten, equalfifteen))
+
+#Exercice D
+wait=0
+max=0
+r=0
+while wait<1:
+    wait=int(input("Veuillez saisir un nombre supérieur a 1 :"))
+while r<wait:
+    max=max+1
+    r=r+max
+if r>wait:
+    print("Le plus grand nombre N est =",max-1)
+    print("")
+else:
+    print("Le plus grand nombre N est =",max)
+    print("")
+
+
+
+
+
+
