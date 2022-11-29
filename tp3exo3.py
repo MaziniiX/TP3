c71fe00e73bb5d547107cf8b4256d7ec9f9bff0a
MaziniiX <bluenderbot@gmail.com>
2022-11-29 13:56:06 +0100

    exo 3 fini

--- tp3exo3.py
+++ tp3exo3.py
@@ -1,1 +1,14 @@
 import random
+essai=1
+nombre=random.randint(0, 100)
+guess=0
+print(nombre)
+while guess!=nombre:
+    guess=int(input("Essayez de deviner le nombre entre 0 et 100:"))
+    if guess<nombre:
+        print("Plus grand!")
+        essai = essai + 1
+    elif guess>nombre:
+        print("Plus petit!")
+        essai=essai+1
+print("Vous avez devinez le nombre en",essai,"essais")
